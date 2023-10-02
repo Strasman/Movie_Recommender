106248b7a307904cec018ca22a59dce6a6898548
Strasman <elad.strasman@gmail.com>
2023-10-02 21:24:01 +0300

    pre-train recommendation

--- recommender/movierecommender/management/commands/make_recommendations.py
+++ recommender/movierecommender/management/commands/make_recommendations.py
@@ -9,10 +9,20 @@ def check_valid_genres(genres: str) -> bool:
     else:
         return False
 
-# Add a Jaccard similarity method here
+# Method to calculate Jaccard Similarity
+def jaccard_similarity(list1: list, list2: list) -> float:
+    s1 = set(list1)
+    s2 = set(list2)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
 
-# Add a movie similarity method here
-
+# Calculate the similarity between two movies
+def similarity_between_movies(movie1: Movie, movie2: Movie) -> float:
+    if check_valid_genres(movie1.genres) and check_valid_genres(movie2.genres):
+        m1_generes = movie1.genres.split()
+        m2_generes = movie2.genres.split()
+        return jaccard_similarity(m1_generes, m2_generes)
+    else:
+        return 0
 
 class Command(BaseCommand):
     help = 'Recommend movies'
